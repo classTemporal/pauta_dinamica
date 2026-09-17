1aab39f69eb615ea320cc1ff36d48cbca0e7abff
Angel Pacheco <classTemporal@users.noreply.github.com>
2026-09-17 16:32:37 -0700

    fix: Enter disparada en login ejecuta LoginCommand (PasswordBox/ComboBox/Window)

--- Views/LoginWindow.xaml.cs
+++ Views/LoginWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using PautaDinamicaApp.ViewModels;
 using Application = System.Windows.Application;
 
@@ -36,6 +37,19 @@ namespace PautaDinamicaApp.Views
             AdminRestoreBox.PasswordChanged += (s, e) => vm.AdminPassword = AdminRestoreBox.Password;
             AdminDeleteBox.PasswordChanged += (s, e) => vm.AdminPassword = AdminDeleteBox.Password;
             ResetNewPasswordBox.PasswordChanged += (s, e) => vm.ResetNewPassword = ResetNewPasswordBox.Password;
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            // Disparar LoginCommand al presionar Enter (PasswordBox/ComboBox/Window)
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                if (DataContext is LoginViewModel vm && vm.LoginCommand.CanExecute(null))
+                {
+                    vm.LoginCommand.Execute(null);
+                    e.Handled = true;
+                }
+            }
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
